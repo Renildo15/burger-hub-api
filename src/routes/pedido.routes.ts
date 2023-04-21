b985d45511773c86ace54e4f8f7b91ece33752cf
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 16:15:43 -0300

    feat: criando rota delete #3

--- src/routes/pedido.routes.ts
+++ src/routes/pedido.routes.ts
@@ -2,17 +2,19 @@ import { Router } from "express";
 import { CreatePedidoController } from "../controllers/pedido/CreatePedidoController";
 import { GetAllPedidoController } from "../controllers/pedido/GetAllPedidoController";
 import { GetPedidoByClienteController } from "../controllers/pedido/GetPedidoByClienteController";
-
+import { DeletePedidoController } from "../controllers/pedido/DeletePedidoController";
 
 const createPedidoController = new CreatePedidoController();
 const getAllPedidoController = new GetAllPedidoController();
 const getPedidoByClienteController = new GetPedidoByClienteController();
+const deletePedidoController = new DeletePedidoController();
 
 const pedidoRoutes = Router();
 
 pedidoRoutes.post("/create", createPedidoController.handle);
 pedidoRoutes.get("/getall", getAllPedidoController.handle);
 pedidoRoutes.get("/:username", getPedidoByClienteController.handle);
+pedidoRoutes.delete("/:username/:id", deletePedidoController.handle);
 
 
 
