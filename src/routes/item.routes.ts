4c5920c9bea7b0f170a914976b6f50e588851b82
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 20:36:19 -0300

    feat: adicionand rota get #5

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { CreateItemController } from "../controllers/item/CreateItemController";
-
+import { GetAllItensController } from "../controllers/item/GetAllItensController";
 
 const createItemController = new CreateItemController();
+const grtAllItensController = new GetAllItensController();
 
 const itemRoutes = Router();
 
 
 itemRoutes.post("/create", createItemController.handle);
+itemRoutes.get("/getall", grtAllItensController.handle);
 
 export default itemRoutes;
