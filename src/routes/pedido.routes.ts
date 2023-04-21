ef11e8dd28a5237f5fc86fc8cae6fc0bab459daa
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 15:38:40 -0300

    feat: criando rota get para getPedidoByCliente #3

--- src/routes/pedido.routes.ts
+++ src/routes/pedido.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import { CreatePedidoController } from "../controllers/pedido/CreatePedidoController";
 import { GetAllPedidoController } from "../controllers/pedido/GetAllPedidoController";
+import { GetPedidoByClienteController } from "../controllers/pedido/GetPedidoByClienteController";
 
 
 const createPedidoController = new CreatePedidoController();
 const getAllPedidoController = new GetAllPedidoController();
+const getPedidoByClienteController = new GetPedidoByClienteController();
 
 const pedidoRoutes = Router();
 
 pedidoRoutes.post("/create", createPedidoController.handle);
-pedidoRoutes.get("/getall", getAllPedidoController.handle)
+pedidoRoutes.get("/getall", getAllPedidoController.handle);
+pedidoRoutes.get("/:username", getPedidoByClienteController.handle);
 
 
 
