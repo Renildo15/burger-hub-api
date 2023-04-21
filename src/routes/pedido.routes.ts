ac24c4499e45ae303a2028a389a33def399b4c39
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 15:11:38 -0300

    feat: adicionando rota get #3

--- src/routes/pedido.routes.ts
+++ src/routes/pedido.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { CreatePedidoController } from "../controllers/pedido/CreatePedidoController";
-
+import { GetAllPedidoController } from "../controllers/pedido/GetAllPedidoController";
 
 
 const createPedidoController = new CreatePedidoController();
-
+const getAllPedidoController = new GetAllPedidoController();
 
 const pedidoRoutes = Router();
 
 pedidoRoutes.post("/create", createPedidoController.handle);
+pedidoRoutes.get("/getall", getAllPedidoController.handle)
 
 
 
