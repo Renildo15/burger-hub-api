2d3258cb73415032812fff4d0e347ae6db33f1d1
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 15:10:30 -0300

    feat: criando getAllPedidos #3

---
+++ src/modules/clientes/useCases/createPedido/GetAllPedidosUseCase.ts
@@ -0,0 +1,8 @@
+import { Pedido } from "@prisma/client";
+import { prisma } from "../../../../prisma/client";
+
+export class GetAllPedidoUseCase{
+    async execute(): Promise<Pedido[]>{
+        return await prisma.pedido.findMany();
+    }
+}
