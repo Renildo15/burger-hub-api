fa9671fce12ec55420513d54745f7ddb726205b4
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 12:25:54 -0300

    feat: adicionando rota delete #1

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateClienteController} from "../controllers/cliente/CreateClienteController";
 import { GetAllClientesController } from "../controllers/cliente/GetAllClienteController";
 import { GetClienteController } from "../controllers/cliente/GetClienteController";
+import { DeleteClienteController } from "../controllers/cliente/DeleteClienteController";
 
 
 
@@ -9,12 +10,14 @@ import { GetClienteController } from "../controllers/cliente/GetClienteControlle
 const createClienteController = new CreateClienteController();
 const getAllClienteController = new GetAllClientesController();
 const getClienteController = new GetClienteController();
+const deleteClienteController = new DeleteClienteController();
 
 const clienteRoutes = Router();
 
 clienteRoutes.post("/create",createClienteController.handle);
 clienteRoutes.get("/getall", getAllClienteController.handle);
 clienteRoutes.get("/:username", getClienteController.handle);
+clienteRoutes.delete("/:username", deleteClienteController.handle);
 
 
 export default clienteRoutes
