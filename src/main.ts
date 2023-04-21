81429c66da7654b5afa7aac7419ae41910d41574
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 11:29:26 -0300

    feat: adicionando rotas de clientes #1

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors"
-import { routes } from "./routes";
+import clienteRoutes from "./routes/cliente.routes";
 import { AppError } from "./errors/AppError";
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json());
-
-app.use(routes);
+app.use("/clientes", clienteRoutes)
 app.use((error: Error, request: Request, response: Response, next: NextFunction)=>{
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
