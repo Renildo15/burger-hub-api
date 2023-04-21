1fedcdca3dd9d16989d27a4b1261da4fe7703c33
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 14:50:05 -0300

    feat/ adicionando rotas na main #3

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors"
 import clienteRoutes from "./routes/cliente.routes";
+import pedidoRoutes from "./routes/pedido.routes";
 import { AppError } from "./errors/AppError";
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json());
-app.use("/clientes", clienteRoutes)
+app.use("/clientes", clienteRoutes);
+app.use("/pedidos", pedidoRoutes);
 app.use((error: Error, request: Request, response: Response, next: NextFunction)=>{
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
