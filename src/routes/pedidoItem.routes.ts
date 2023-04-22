ecda864a4b1ed2f2a06a2c44390d5df8ad7ca134
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-22 09:56:18 -0300

    feat: adicionando rota delete #7

--- src/routes/pedidoItem.routes.ts
+++ src/routes/pedidoItem.routes.ts
@@ -2,17 +2,18 @@ import { Router } from "express";
 import { CreatePedidoItemController } from "../controllers/pedidoItem/CreatePedidoItemController";
 import { GetAllPedidoItensController } from "../controllers/pedidoItem/GetAllPedidoItensController";
 import { GetPedidoItemController } from "../controllers/pedidoItem/GetPedidoItemController";
-
+import { DeletePedidoItemController } from "../controllers/pedidoItem/DeletePedidoItemController";
 
 const createPedidoItemController = new CreatePedidoItemController();
 const getAllPedidoItensController = new GetAllPedidoItensController();
 const getPedidoItemController = new GetPedidoItemController();
-
+const deletePedidoItemController =  new DeletePedidoItemController();
 
 const pedidoItemRoutes = Router();
 
 pedidoItemRoutes.post("/create", createPedidoItemController.handle);
 pedidoItemRoutes.get("/getall", getAllPedidoItensController.handle);
 pedidoItemRoutes.get("/:id", getPedidoItemController.handle);
+pedidoItemRoutes.delete("/:id", deletePedidoItemController.handle)
 
 export default pedidoItemRoutes
