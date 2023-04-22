406c38e8e1d3e901274da1a8b641a5f2ae5dc022
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-21 21:20:13 -0300

    feat: criando rota delete #5

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { CreateItemController } from "../controllers/item/CreateItemController";
 import { GetAllItensController } from "../controllers/item/GetAllItensController";
 import { GetItemController } from "../controllers/item/GetItemController";
+import { DeleteItemController } from "../controllers/item/DeleteItemController";
 
 const createItemController = new CreateItemController();
 const getAllItensController = new GetAllItensController();
 const getItemController = new GetItemController();
+const deleteItemController = new DeleteItemController();
 
 const itemRoutes = Router();
 
@@ -13,5 +15,6 @@ const itemRoutes = Router();
 itemRoutes.post("/create", createItemController.handle);
 itemRoutes.get("/getall", getAllItensController.handle);
 itemRoutes.get("/:nome", getItemController.handle);
+itemRoutes.delete("/:nome", deleteItemController.handle);
 
 export default itemRoutes;
