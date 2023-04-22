bec9cd852554a5e250b83728f8e56217f5df9cf5
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-22 10:18:43 -0300

    feat: criando rota patch #7

--- src/routes/pedidoItem.routes.ts
+++ src/routes/pedidoItem.routes.ts
@@ -3,17 +3,20 @@ import { CreatePedidoItemController } from "../controllers/pedidoItem/CreatePedi
 import { GetAllPedidoItensController } from "../controllers/pedidoItem/GetAllPedidoItensController";
 import { GetPedidoItemController } from "../controllers/pedidoItem/GetPedidoItemController";
 import { DeletePedidoItemController } from "../controllers/pedidoItem/DeletePedidoItemController";
+import { UpdatePedidoItemController } from "../controllers/pedidoItem/UpdatePedidoItemController";
 
 const createPedidoItemController = new CreatePedidoItemController();
 const getAllPedidoItensController = new GetAllPedidoItensController();
 const getPedidoItemController = new GetPedidoItemController();
 const deletePedidoItemController =  new DeletePedidoItemController();
+const updatePedidoItemController = new UpdatePedidoItemController();
 
 const pedidoItemRoutes = Router();
 
 pedidoItemRoutes.post("/create", createPedidoItemController.handle);
 pedidoItemRoutes.get("/getall", getAllPedidoItensController.handle);
 pedidoItemRoutes.get("/:id", getPedidoItemController.handle);
-pedidoItemRoutes.delete("/:id", deletePedidoItemController.handle)
+pedidoItemRoutes.delete("/:id", deletePedidoItemController.handle);
+pedidoItemRoutes.patch("/update/:id", updatePedidoItemController.handle);
 
 export default pedidoItemRoutes
